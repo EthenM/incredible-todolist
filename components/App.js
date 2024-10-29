ed66856181750d58c3d785fe0f7cba66106fed65
Ethen Mitterhuber <ethenmitterhuber@gmail.com>
2024-10-29 11:56:21 -0600

    Lab 3 completed.

--- components/App.js
+++ components/App.js
@@ -1,15 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View } from 'react-native'
+import React, { useState } from 'react'
 import Todolist from './Todolist'
 import Todoform from './Todoform'
 
 const App = () => {
+  const [tasks, setTasks] = useState(['Do laundry', 'Go to gym', 'Walk dog']);
+
   return (
     <View>
-      <Todolist/>
+      <Todolist tasks={tasks}/>
       <Todoform/>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
