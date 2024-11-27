e2823573b2e6e44732b9b52d094128553fc7f263
Ethen Mitterhuber <ethenmitterhuber@gmail.com>
2024-11-27 14:13:30 -0700

    Added back the todolist stuff.

--- app/App.js
+++ app/App.js
@@ -1,30 +1,41 @@
-import { View, Text } from 'react-native';
-import React, { useState } from 'react';
-import Todolist from './components/Todolist';
-import TodoForm from './components/Todoform';
-import { NavigationContainer, NavigationIndependentTree } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './Screens/HomeScreen';
-import AboutScreen from './Screens/AboutScreen';
+import { View, Text } from "react-native";
+import React, { useState } from "react";
+import Todolist from "./components/Todolist";
+import TodoForm from "./components/Todoform";
+import { NavigationContainer, NavigationIndependentTree } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./Screens/HomeScreen";
+import AboutScreen from "./Screens/AboutScreen";
 
 const App = () => {
-  const [tasks, setTasks] = useState(['Do laundry', 'Go to gym', 'Walk dog']);
-  // const Stack = createStackNavigator();
-  
-  const addTask = (taskText) => {
-    setTasks([...tasks, taskText])
-  }
+    const [tasks, setTasks] = useState(["Do laundry", "Go to gym", "Walk dog"]);
+    // const Stack = createStackNavigator();
 
+    const addTask = taskText => {
+        setTasks([...tasks, taskText]);
+    };
 
-  return (
-    <NavigationIndependentTree>
-        <NavigationContainer>
-          
-          {/* todolist code */}
-          
-      </NavigationContainer>
-    </NavigationIndependentTree>
-  );
+    return (
+        <NavigationIndependentTree>
+            <NavigationContainer>
+
+                
+
+                {/* todolist code */}
+                <View>
+                    <View
+                        style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Todolist tasks={tasks} />
+                    </View>
+                    <TodoForm addTask={addTask} />
+                </View>
+            </NavigationContainer>
+        </NavigationIndependentTree>
+    );
 };
 
 export default App;
